06ddda8612bc3e1b3d4834531124b8c56c6203df
Sam <samnafea705@gmail.com>
2026-02-15 15:57:13 +0100

    Darstellung von Fragen und Antworten Für SingleChoice-Page in separaten Cards.(#17) (#18)
    * Added Classes (Frage + Antowrt) and Service (Frage). and Design (Frage Single Choice).
    
    ---------
    
    Co-authored-by: sna <samir.nafeaali@quality-automation.de>

---
+++ Schulprojekt/Data/Frage.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Schulprojekt.Data
+{
+    public class Frage
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? EndText { get; set; }
+
+        [InverseProperty("Frage")]
+        public virtual ICollection<Antwort> Antworten { get; set; } = new List<Antwort>();
+        
+        [NotMapped]
+        public Antwort? SelectedAntwort { get; set; }
+    }
+}
