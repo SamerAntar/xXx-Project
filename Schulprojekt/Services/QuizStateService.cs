94475a48bcaff8c9073a4f7a0952b59f65890f19
sna <samir.nafeaali@quality-automation.de>
2026-02-23 20:57:37 +0100

    Bugfix: Endlos-Quiz Schleife.(#38)

---
+++ Schulprojekt/Services/QuizStateService.cs
@@ -0,0 +1,8 @@
+﻿namespace Schulprojekt.Services
+{
+    public class QuizStateService
+    {
+        public HashSet<int> RemainingQuizIds { get; set; } = new();
+        public bool IsInitialized { get; set; } = false;
+    }
+}
