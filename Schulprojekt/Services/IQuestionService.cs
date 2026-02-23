26133c9e6fd3b8dbcde3de981e7242c81d5cb8a9
Joel <joel.mbanza@cancom.de>
2026-02-23 09:04:32 +0100

    IQuestionServiceKommentare#33

--- Schulprojekt/Services/IQuestionService.cs
+++ Schulprojekt/Services/IQuestionService.cs
@@ -2,9 +2,13 @@
 
 namespace Schulprojekt.Services
 {
+    // Interface für das Lesen von Questions inkl. Navigationseigenschaften
     public interface IQuestionService
     {
+        // Lädt alle Questions inkl. verknüpfter Daten (z. B. Answers)
         Task<IEnumerable<Question>> GetAllEntriesIncludingNavigationsAsync();
+
+        // Lädt alle Questions eines bestimmten QuestionSets inkl. Navigationen
         Task<IEnumerable<Question>> GetAllEntriesByQuestionSetIncludingNavigationsAsync(int questionSetID);
     }
 }
