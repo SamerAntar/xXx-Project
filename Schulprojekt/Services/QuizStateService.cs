74e63767ab83263af071a2b409a2efa51063f292
Sam <samnafea705@gmail.com>
2026-02-25 22:38:24 +0100

    Fix Bug: endlos quiz.(#38) (#39)
    * Bugfix: Endlos-Quiz Schleife.(#38)
    
    ---------
    
    Co-authored-by: sna <samir.nafeaali@quality-automation.de>

---
+++ Schulprojekt/Services/QuizStateService.cs
@@ -0,0 +1,14 @@
+﻿namespace Schulprojekt.Services
+{
+    public class QuizStateService
+    {
+        public HashSet<int> RemainingQuizIds { get; set; } = new();
+        public bool IsInitialized { get; set; } = false;
+
+        public void Reset()
+        {
+            RemainingQuizIds.Clear();
+            IsInitialized = false;
+        }
+    }
+}
