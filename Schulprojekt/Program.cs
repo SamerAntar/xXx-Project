e84c611f23afce00ffe5274314536dd0f80bb67c
sna <samir.nafeaali@quality-automation.de>
2026-03-02 08:45:34 +0100

    Backup.(#46)

--- Schulprojekt/Program.cs
+++ Schulprojekt/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddScoped<IQuestionService, QuestionService>();
 builder.Services.AddScoped<IThemaService, ThemaService>();
 builder.Services.AddScoped<ISpielerService, SpielerService>();
 builder.Services.AddScoped<QuizStateService>();
+builder.Services.AddScoped<FinalResultStateService>();
 builder.Services.AddRadzenComponents();
 
 
