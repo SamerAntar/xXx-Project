0b2b39b55ec4012382a8be4ec27ce0b7306d6566
Sam <samnafea705@gmail.com>
2026-03-14 16:58:51 +0100

    Add Character table and link Character to QuestionSetProgress.(#67) (#68)
    Co-authored-by: sna <samir.nafeaali@quality-automation.de>

--- Schulprojekt/Data/Thema.cs
+++ Schulprojekt/Data/Thema.cs
@@ -9,6 +9,8 @@ namespace Schulprojekt.Data
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string? Description { get; set; }
+        public string? GamePlaceName { get; set; }
+        public string? GamePlaceDescription { get; set; }
 
         [InverseProperty("Thema")]
         public ICollection<QuestionSet> QuestionSets { get; set; } = new List<QuestionSet>();
