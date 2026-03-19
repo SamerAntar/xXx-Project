82378d6b957bdca0c5270317d802764c02bc2e7e
sna <samir.nafeaali@quality-automation.de>
2026-03-19 20:27:54 +0100

    Adjusted_QuestionsData

---
+++ Schulprojekt/Migrations/20260319192721_Adjusted_QuestionsData.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Schulprojekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adjusted_QuestionsData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+               table: "McAnswers",
+               keyColumn: "Id",
+               keyValue: 42,
+               column: "OptionText",
+               value: "Ein alt-Fragment benötigt mindestens zwei Akteure"
+           );
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "StartText",
+                value: "Welche Beziehung wird im UML-Klassendiagramm durch eine durchgezogene Linie mit einer ausgefüllten Raute dargestellt?"
+            );
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 19,
+                column: "QuestionType",
+                value: "GAP"
+            );
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 20,
+                column: "QuestionType",
+                value: "GAP"
+            );
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 29,
+                column: "QuestionType",
+                value: "GAP"
+            );
+
+            migrationBuilder.UpdateData(
+                table: "Questions",
+                keyColumn: "Id",
+                keyValue: 30,
+                column: "QuestionType",
+                value: "GAP"
+            );
+
+            migrationBuilder.UpdateData(
+                table: "QuestionSets",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Title",
+                value: "Klassendiagramm Quiz"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
