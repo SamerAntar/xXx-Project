fb96e8bf91890bde80ff570bfdb0a9be475214ab
sna <samir.nafeaali@quality-automation.de>
2026-03-19 20:09:34 +0100

    Seeder-Add_SingleChoice_10

---
+++ Schulprojekt/Migrations/20260319190859_Add_SingleChoice_10.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Schulprojekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_SingleChoice_10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //Questions
+            migrationBuilder.InsertData(
+                table: "Questions",
+                columns: new[] { "Id", "StartText", "QuestionType", "QuestionSetId", "AllowsMultiple" },
+                values: new object[,]
+                {
+                    {
+                        10,
+                        "Eine Aggregation ist stärker als eine Komposition.",
+                        "TF",
+                        1,
+                        false
+                    }
+                }
+            );
+
+            //MC Answers
+            migrationBuilder.InsertData(
+                table: "McAnswers",
+                columns: new[] { "Id", "QuestionId", "OptionText", "Points", "IsCorrect", "OptionOrder" },
+                values: new object[,]
+                {
+                    { 25, 10, "Wahr", 0, false, 0 },
+                    { 26, 10, "Falsch", 1, true, 1 },
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
